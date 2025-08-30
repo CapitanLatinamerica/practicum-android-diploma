eceb7aac3df0680f8f06ec283f9e0270f556b58e
KitLeskinen <kawaiicreature@gmail.com>
2025-08-30 10:15:28 +0300

    - Отключен поворот экрана - Минимально поддерживаемая версия Android - Настройка AndroidManifest.xml - Ссылка на класс Application - Добавлены основные библиотеки в Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("convention.detekt")
+}
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.9.3"
+        classpath(libs.navigation.safe.args.gradle.plugin)
+    }
 }
