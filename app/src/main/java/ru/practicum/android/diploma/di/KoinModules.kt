a25b26e77762590e1de82de4677819167d3a7ac3
Capitan Latinamerica <capitan@latinoamerica512.ru>
2025-08-31 20:37:01 +0300

    SS-7/ DI Добавил директорию для модулей Коин. Создал пока всего 3 модуля - appModule, searchModule и databaseModule. Добавил вызов модулей в App

---
+++ app/src/main/java/ru/practicum/android/diploma/di/KoinModules.kt
@@ -0,0 +1,20 @@
+package ru.practicum.android.diploma.di
+
+import org.koin.dsl.module
+
+// Общие зависимости
+val appModule = module {
+
+}
+
+// Модуль для работы с Room
+val databaseModule = module {
+
+}
+
+// Модуль для работы с Room
+val searchModule = module {
+
+}
+
+
