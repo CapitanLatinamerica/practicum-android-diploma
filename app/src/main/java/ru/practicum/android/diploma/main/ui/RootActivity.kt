3342827d217658cb8fa90273aa2ea3d623b6917f
STer <igorrozembaum@yandex.ru>
2025-09-01 15:40:59 +0300

    SS-10/NavController связан с панелью навигации: - Связаны BottomNavigationView и NavController в RootActivity - Реализован ViewBinding в Fragment с инициализацией в onCreateView для проверки работы навигации - Переименован id у FragmentContainerView

--- app/src/main/java/ru/practicum/android/diploma/main/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/main/ui/RootActivity.kt
@@ -2,16 +2,29 @@ package ru.practicum.android.diploma.main.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.API_ACCESS_TOKEN)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
     private fun networkRequestExample(accessToken: String) {
