00a102ff5e3495079dc4a7831c5995438ba9525a
KitLeskinen <kawaiicreature@gmail.com>
2025-09-05 09:35:07 +0300

    - Исправления

--- app/src/main/java/ru/practicum/android/diploma/search/domain/SearchVacanciesUseCase.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/SearchVacanciesUseCase.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.search.domain
 import ru.practicum.android.diploma.common.data.model.NetworkClient
 import ru.practicum.android.diploma.common.data.model.VacanciesRequest
 
-
 class SearchVacanciesUseCase(private val networkClient: NetworkClient) {
     suspend fun execute() {
         networkClient.doRequest(VacanciesRequest())
