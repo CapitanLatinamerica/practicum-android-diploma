d34fe2a4fddff443137dd39c4bf5a5921ec58b58
CapitanLatinamerica <capitan@latinoamerica512.ru>
2025-09-06 18:39:58 +0300

    Update VacancyDetailsRepository.kt

--- app/src/main/java/ru/practicum/android/diploma/vacancydetails/domain/VacancyDetailsRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancydetails/domain/VacancyDetailsRepository.kt
@@ -2,7 +2,4 @@ package ru.practicum.android.diploma.vacancydetails.domain
 
 interface VacancyDetailsRepository {
     suspend fun getVacancyDetails(vacancyId: String): VacancyDetails
-    suspend fun addToFavorites(vacancy: VacancyDetails)
-    suspend fun removeFromFavorites(vacancyId: String)
-    suspend fun isVacancyFavorite(vacancyId: String): Boolean
 }
