76ef4c55fc9a69f38a560af4d44be4b4279c2d94
KitLeskinen <kawaiicreature@gmail.com>
2025-09-06 23:10:35 +0300

    - Исправления форматирования кода

--- app/src/test/java/ru/practicum/android/diploma/common/data/mapper/AreaMapperTest.kt
+++ app/src/test/java/ru/practicum/android/diploma/common/data/mapper/AreaMapperTest.kt
@@ -8,30 +8,20 @@ class AreaMapperTest {
     @Test
     fun should_Map_Area_Dto_To_Area() {
 
-        val areaDtoFirstChild = AreaDto(
-            areas = null,
-            id = "1",
-            name = "Ереван",
-            parentId = "5"
-        )
-        val areaDtoSecondChild = AreaDto(
-            areas = null,
-            id = "2",
-            name = "Гюмри",
-            parentId = "5"
-        )
+        val areaDtoFirstChild = AreaDto(areas = null, id = "1", name = "Ереван", parentId = "5")
+        val areaDtoSecondChild = AreaDto(areas = null, id = "2", name = "Гюмри", parentId = "5")
 
-        val areaDtoParent = AreaDto(
-            areas = listOf(areaDtoFirstChild, areaDtoSecondChild),
-            id = "5",
-            name = "Армения",
-            parentId = null
-        )
+        val areaDtoParent =
+            AreaDto(
+                areas = listOf(areaDtoFirstChild, areaDtoSecondChild),
+                id = "5",
+                name = "Армения",
+                parentId = null,
+            )
 
         val area = AreaMapper.mapAreaDtoToArea(areaDtoParent)
 
         assertEquals(1, area.areas[0].id)
         assertEquals(2, area.areas[1].id)
     }
-
 }
