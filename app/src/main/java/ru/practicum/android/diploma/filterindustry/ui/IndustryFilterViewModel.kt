10deb7d74fdde2f3fe80aa19310c2f5590ce53f3
Capitan Latinamerica <capitan@latinoamerica512.ru>
2025-09-12 10:10:17 +0300

    SS-71/ Создал фрагмент отраслей. Коммит 1

---
+++ app/src/main/java/ru/practicum/android/diploma/filterindustry/ui/IndustryFilterViewModel.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.filterindustry.ui
+
+import androidx.lifecycle.ViewModel
+
+class IndustryFilterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
